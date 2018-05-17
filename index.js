0a54be135ee065f2fb427c032168f7867eb65fc0
Jennifer Dobak <jdobak1@gmail.com>
2018-05-17 00:30:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const app = "I don't do much.";
-
-fetch ('https://api.github.com/repos/jquery/jquery/commits')
-.then(res => res.json())
-.then(json => console.log(json))
+const token = '8370aa523329cc0665ce0372f9bb01066d875a54'
+fetch ('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json))
